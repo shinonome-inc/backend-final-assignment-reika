d35332f345990df6e9716d0baa2abc2d79915700
REIKA <reikaseki2005@gmail.com>
2024-05-24 18:40:25 +0900

    変更

--- mysite/urls.py
+++ mysite/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path("accounts/", include("accounts.urls")),
     path("tweets/", include("tweets.urls")),
     path("", include("welcome.urls")),
-    path('blog/', include('blog.urls')),
+
 ]
