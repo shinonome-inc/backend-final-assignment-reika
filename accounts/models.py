42b0b2f87f6047749af970a6cea22003495d53c6
REIKA <reikaseki2005@gmail.com>
2024-06-05 08:28:07 +0900

    修正

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,10 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
 
 
 class User(AbstractUser):
     email = models.EmailField()
+    groups = models.ManyToManyField(Group, related_name="custom_user_set", blank=True)  # related_nameを追加
+    user_permissions = models.ManyToManyField(
+        Permission, related_name="custom_user_permissions_set", blank=True  # related_nameを追加
+    )
