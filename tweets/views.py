bd8bba77725685e3dd89d3c763c3229592c19c06
REIKA <reikaseki2005@gmail.com>
2024-08-19 16:01:37 +0900

    テスト修正

--- tweets/views.py
+++ tweets/views.py
@@ -1,3 +1,5 @@
+
+
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, DeleteView, DetailView, ListView
